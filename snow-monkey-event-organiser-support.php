df6d3e6baa74c60c2aeb412341ec33e2e70f97f9
inc2734 <inc@2inc.org>
2024-11-12 18:15:29 +0900

    WordPress 6.7 対応

--- snow-monkey-event-organiser-support.php
+++ snow-monkey-event-organiser-support.php
@@ -40,12 +40,7 @@ class Bootstrap {
 	 * Plugins loaded.
 	 */
 	public function _plugins_loaded() {
-		load_plugin_textdomain(
-			'snow-monkey-event-organiser-support',
-			false,
-			basename( __DIR__ ) . '/languages'
-		);
-
+		add_action( 'init', array( $this, '_load_textdomain' ) );
 		add_action( 'init', array( $this, '_activate_autoupdate' ) );
 
 		$theme = wp_get_theme( get_template() );
@@ -123,6 +118,17 @@ class Bootstrap {
 			function () {
 				new Controller\Front();
 			}
+		);
+	}
+
+	/**
+	 * Load textdomain.
+	 */
+	public function _load_textdomain() {
+		load_plugin_textdomain(
+			'snow-monkey-event-organiser-support',
+			false,
+			basename( __DIR__ ) . '/languages'
 		);
 	}
 
